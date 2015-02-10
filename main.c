b19cd59fa78b171f71342b8d1783956d20f00ec3
mboerwinkle <mboerwinkle@gmail.com>
2015-02-10 15:33:30 +0100

    anaglyph?

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <SDL2/SDL.h>
 #include <time.h>
 #include "globals.h"
+#define ANAGLYPH
 
 static SDL_Window* window;
 static SDL_Renderer* render;
@@ -29,6 +30,10 @@ int main(){
 	tetrahedron->rot[0] = 0.5;
 	tetrahedron->rot[1] = 2;
 	tetrahedron->rot[2] = 1;
+	square->rotationspeed = 0.01;
+	square->rot[0] = 3;
+	square->rot[1] = 1;
+	square->rot[2] = 0;
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 	window = SDL_CreateWindow("Relign, BITCHES! Finally used that name! BLAM!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, 0);
 	if(window == NULL){
@@ -48,6 +53,24 @@ int main(){
 			x = mesh1->centermass[0];
 			y = mesh1->centermass[1];
 			z = mesh1->centermass[2];
+			#ifdef ANAGLYPH
+			for(raycounter = mesh1->rays-1; raycounter >= 0; raycounter--){
+				x1 = x + mesh1->pointmatrix[mesh1->raymatrix[raycounter].ends[0]*3+0];
+				y1 = y + mesh1->pointmatrix[mesh1->raymatrix[raycounter].ends[0]*3+1];
+				z1 = z + mesh1->pointmatrix[mesh1->raymatrix[raycounter].ends[0]*3+2];
+				x2 = x + mesh1->pointmatrix[mesh1->raymatrix[raycounter].ends[1]*3+0];
+				y2 = y + mesh1->pointmatrix[mesh1->raymatrix[raycounter].ends[1]*3+1];
+				z2 = z + mesh1->pointmatrix[mesh1->raymatrix[raycounter].ends[1]*3+2];
+				SDL_SetRenderDrawColor(render, 255, 0, 0, 125);
+				SDL_RenderDrawLine(render, (int)((100*x1-50)/z1)+250, (int)(100*y1/z1)+250, (int)((100*x2-50)/z2)+250, (int)(100*y2/z2)+250);
+				SDL_SetRenderDrawColor(render, 0, 0, 255, 125);
+				SDL_RenderDrawLine(render, (int)((100*x1+50)/z1)+250, (int)(100*y1/z1)+250, (int)((100*x2+50)/z2)+250, (int)(100*y2/z2)+250);
+			}
+			SDL_SetRenderDrawColor(render, 255, 0, 0, 125);
+			SDL_RenderDrawPoint(render, (int)((100*x-50)/z+250), (int)(y*100/z+250));
+			SDL_SetRenderDrawColor(render, 0, 0, 255, 125);
+			SDL_RenderDrawPoint(render, (int)((100*x+50)/z+250), (int)(y*100/z+250));
+				#else
 			if(!color) SDL_SetRenderDrawColor(render, 10, 255, 55, 255);
 			else SDL_SetRenderDrawColor(render, 255, 10, 10, 255);
 			for(raycounter = mesh1->rays-1; raycounter >= 0; raycounter--){
@@ -61,14 +84,15 @@ int main(){
 			}
 			SDL_SetRenderDrawColor(render, 100, 100, 255, 255);
 			SDL_RenderDrawPoint(render, (int)(x*100/z+250), (int)(y*100/z+250));
+			#endif
 		}
 		paint();
 		SDL_SetRenderDrawColor(render, 0, 0, 0, 100);
 		SDL_RenderClear(render);
 		SDL_Event evnt;
-//		do {
+		do {
 			SDL_PollEvent(&evnt);
-//		} while (evnt.type != SDL_KEYDOWN && evnt.type != SDL_QUIT);
+		} while (evnt.type != SDL_KEYDOWN && evnt.type != SDL_QUIT);
 		if (evnt.type == SDL_QUIT){
 			running = 0;
 		}
