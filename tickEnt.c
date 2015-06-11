1978ca81681f71b546b1ccb01ccb62e87e9014e7
Simon Boerwinkle <sboerwinkle@gmail.com>
2015-06-11 11:51:57 -0500

    Fixed a minor warning in tickEnt compilation

--- tickEnt.c
+++ tickEnt.c
@@ -2,14 +2,6 @@
 #include <stdlib.h>
 #include "globals.h"
 
-void tickAllEnts(){
-	ent* target;
-		SDL_SetRenderDrawColor(render, 255, 255, 0, 255);
-	for(int temp = entcount-1; temp >= 0; temp--){
-		tickEnt(ents[temp]);
-	}
-}
-
 void tickEnt(ent *target){
 	double head[3];
 	head[0] = target->center[0];
@@ -19,5 +11,13 @@ void tickEnt(ent *target){
 	target->aiFunc(target);
 	target->typeFunc(target);
 	
+}
+
+void tickAllEnts(){
+	ent* target;
+		SDL_SetRenderDrawColor(render, 255, 255, 0, 255);
+	for(int temp = entcount-1; temp >= 0; temp--){
+		tickEnt(ents[temp]);
+	}
 }
 
